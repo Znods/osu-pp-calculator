86f32cfd49d81f09b263145bef6a3bf37dc47ecf
Znods <70346380+Znods@users.noreply.github.com>
2022-10-24 10:05:40 -0500

    Delete twitch.h

--- headers/twitch.h
+++
@@ -1,6 +0,0 @@
-
-
-int twitch_socket();
-void twitch_login(int, char *, char *, char *);
-void ping_check(int, char *);
-void commands(int, char *, char *, struct beatmap *, struct beatmap_data *);
