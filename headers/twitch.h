560c721f662fe7f5fd00c6b66636251d3e59cdf2
Znods <gaotiihd@gmail.com>
2022-10-26 00:27:54 +0000

    patch 1.3.1

--- headers/twitch.h
+++ headers/twitch.h
@@ -2,6 +2,6 @@
 #include <stdbool.h>
 
 int twitch_socket();
-void twitch_login(int, char *, char *, char *);
+int twitch_login(int, char *, char *, char *);
 void ping_check(int, char *);
 bool commands(int, char *, char *, struct beatmap *, struct beatmap_data *, char *);
